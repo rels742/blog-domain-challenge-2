38a95b74728d025e4f642962824b2d4a46ea819e
Lorel <lorel.oboue@gmail.com>
2022-09-01 12:21:05 +0100

    requirement two complete

---
+++ prisma/seed.js
@@ -0,0 +1,12 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
