ee9e3d2d0f2c0beb8353b9c90353fcf8c913846e
Lorel <lorel.oboue@gmail.com>
2022-09-01 12:51:10 +0100

    user model and seed complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,21 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-main()
+async function seed() {
+  const createdUser = await prisma.user.create({
+    data: {
+      username: "Alice",
+      email: "alice@hotgmail.com",
+      password: "blog123",
+    },
+  });
+
+  console.log("Customer created", createdUser);
+
+  process.exit(0);
+}
+
+seed()
   .then(async () => {
     await prisma.$disconnect();
   })
