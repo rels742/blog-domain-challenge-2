c44ecd44ae97a10294a1b5b048ddc1f426b3e3be
Lorel <lorel.oboue@gmail.com>
2022-09-02 17:23:10 +0100

    user post error 500 added and working

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,17 +2,34 @@ const { Prisma } = require("@prisma/client");
 const prisma = require("../utils/prisma");
 
 const createUser = async (req, res) => {
+  const { username, email, password, firstName, lastName, age, pictureUrl } =
+    req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    !firstName ||
+    !lastName ||
+    !age ||
+    !pictureUrl
+  ) {
+    return res.status(400).json({
+      error: "Missing fields missing in the request body",
+    });
+  }
+
   const createdUser = await prisma.user.create({
     data: {
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
+      username,
+      email,
+      password,
       profile: {
         create: {
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          age: req.body.age,
-          pictureUrl: req.body.pictureUrl,
+          firstName,
+          lastName,
+          age,
+          pictureUrl,
         },
       },
     },
